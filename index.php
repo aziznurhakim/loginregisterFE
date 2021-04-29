7f320c56f09d1deeede781163d39cd28cddae22b
Yudhanjeffri <yudhanjeffri168@gmail.com>
2021-04-29 16:14:32 +0700

    merge main

--- index.php
+++ index.php
@@ -33,14 +33,14 @@
 				<button type="button" class="toggle-btn" onclick="register()">Register</button>
 			</div>
 			<form id="login" class="input-group" method="post" action="cek_login.php">
-				<input type="text" class="input-field" name="username" placeholder="User Id" required>
-				<input type="text" class="input-field" name="password" placeholder="Enter Password" required>
+				<input type="text" class="input-field" name="username" placeholder="Username" required>
+				<input type="password" class="input-field" name="password" placeholder="Enter Password" required>
 				<input type="checkbox" class="chec-box"><span>Remember Password</span>
 				<button type="submit" class="submit-btn">Log In</button>
 			</form>
 			<form id="register" class="input-group" method = "post" action="">
 				<input type="text" class="input-field" for="username" name="username" id="username" placeholder="Username" required>
-				<input type="text" class="input-field" for="password" name="password" id="username" placeholder="Enter Password" required>
+				<input type="password" class="input-field" for="password" name="password" id="username" placeholder="Enter Password" required>
 				<input type="checkbox" class="chec-box"><span>I agree to the terms & conditions</span>
 				<button type="submit" class="submit-btn" name="submitButtonRegis">Register</button>
 			</form>
