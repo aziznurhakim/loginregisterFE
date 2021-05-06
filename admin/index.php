952dd54dcaba91a7f1764303b539279a38cf2f01
Yudhanjeffri <yudhanjeffri168@gmail.com>
2021-05-06 14:11:43 +0700

    change sidebar

--- admin/index.php
+++ admin/index.php
@@ -23,11 +23,9 @@
         <ul>
 			
             <li><a href="#"><i class="fas fa-home"></i>Home</a></li>
-            <li><a href="#"><i class="fas fa-user"></i>Profile</a></li>
-            <li><a href="#"><i class="fas fa-address-card"></i>About</a></li>
-            <li><a href="#"><i class="fas fa-project-diagram"></i>portfolio</a></li>
-            <li><a href="#"><i class="fas fa-blog"></i>Blogs</a></li>
-            <li><a href="#"><i class="fas fa-address-book"></i>Contact</a></li>
+            <li><a href="#"><i class="fas fa-user"></i>Siswa</a></li>
+            <li><a href="#"><i class="fas fa-address-card"></i>Pinjaman</a></li>
+            <li><a href="#"><i class="fas fa-project-diagram"></i>Pengembalian</a></li>
             <li><a href="logout.php"><i class="fas fa-map-pin"></i>Logout</a></li>
         </ul> 
         <div class="social_media">
